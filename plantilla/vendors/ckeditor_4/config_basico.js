02895635b0fa358b4a0cdf9066328e384bb433e8
luis-hernandez01 <58452805+luis-hernandez01@users.noreply.github.com>
2022-11-18 22:26:52 -0500

    Commit_plantilla

---
+++ plantilla/vendors/ckeditor_4/config_basico.js
@@ -0,0 +1,19 @@
+CKEDITOR.editorConfig = function( config ) {
+	config.toolbarGroups = [
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+
+	config.removeButtons = 'Source,Save,NewPage,Preview,Print,Templates,Replace,Find,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,CreateDiv,Language,BidiRtl,BidiLtr,Link,Unlink,EasyImageUpload,Flash,Table,HorizontalRule,Smiley,SpecialChar,Iframe,PageBreak,About,Maximize,ShowBlocks,Styles,Format,Font,Blockquote';
+};
